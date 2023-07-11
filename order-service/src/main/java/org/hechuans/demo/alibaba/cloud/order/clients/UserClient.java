02e2f6c1b9931d90b80e21bdf4f6eed3efde982d
hechuansheng <4rfv$RFV>
2023-07-11 11:01:01 +0800

    feign日志配置

--- order-service/src/main/java/org/hechuans/demo/alibaba/cloud/order/clients/UserClient.java
+++ order-service/src/main/java/org/hechuans/demo/alibaba/cloud/order/clients/UserClient.java
@@ -1,5 +1,6 @@
 package org.hechuans.demo.alibaba.cloud.order.clients;
 
+import org.hechuans.demo.alibaba.cloud.order.config.feign.FeignConfig;
 import org.hechuans.demo.alibaba.cloud.order.vo.UserVO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @date : 2023/7/11 8:37
  * @since : version-1.0
  */
-@FeignClient("user-service")
+@FeignClient(value = "user-service", configuration = FeignConfig.class)
 public interface UserClient {
 
     @GetMapping("/user/getById")
--- order-service/src/main/java/org/hechuans/demo/alibaba/cloud/order/config/feign/FeignConfig.java
+++ order-service/src/main/java/org/hechuans/demo/alibaba/cloud/order/config/feign/FeignConfig.java
@@ -1,16 +1,19 @@
 package org.hechuans.demo.alibaba.cloud.order.config.feign;
 
-import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Configuration;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author : hechuansheng
  * @date : 2023/7/11 8:36
  * @since : version-1.0
  */
-//@EnableFeignClients
-//@AutoConfiguration
+
 public class FeignConfig {
 
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        return Logger.Level.BASIC;
+    }
 }
