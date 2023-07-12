ce6d064c7f88658037ee7c9511c853d37ee73940
hechuansheng <4rfv$RFV>
2023-07-12 08:57:20 +0800

    gateway网关

---
+++ gateway/src/main/java/org/hechuans/demo/alibaba/cloud/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package org.hechuans.demo.alibaba.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : hechuansheng
+ * @date : 2023/7/11 16:54
+ * @since : version-1.0
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
