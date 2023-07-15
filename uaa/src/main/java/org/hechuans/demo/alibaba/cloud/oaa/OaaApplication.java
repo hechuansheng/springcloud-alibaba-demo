f6f51d484ce680c99f3c94dec8b9e480e7eedca0
hechuansheng <4rfv$RFV>
2023-07-15 18:27:21 +0800

    鉴权中心初始化

---
+++ uaa/src/main/java/org/hechuans/demo/alibaba/cloud/oaa/OaaApplication.java
@@ -0,0 +1,18 @@
+package org.hechuans.demo.alibaba.cloud.oaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : hechuansheng
+ * @date : 2023/7/15 17:01
+ * @since : version-1.0
+ */
+@SpringBootApplication
+public class OaaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OaaApplication.class, args);
+    }
+
+}
---
+++ uaa/src/main/java/org/hechuans/demo/alibaba/cloud/oaa/config/WebSecurityConfig.java
@@ -0,0 +1,50 @@
+package org.hechuans.demo.alibaba.cloud.oaa.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * @author : hechuansheng
+ * @date : 2023/7/15 17:35
+ * @since : version-1.0
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain httpSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests.antMatchers("/login").permitAll()
+                                .anyRequest().authenticated()
+                )
+
+                //设置form登录，设置且放开登录页login
+                .formLogin(fromLogin -> fromLogin.loginPage("/login").permitAll())
+
+                // Spring Security CSRF保护
+                .csrf().disable();
+//                .csrf(csrfToken -> csrfToken.csrfTokenRepository(new CookieCsrfTokenRepository()))
+//                 //开启认证服务器的资源服务器相关功能，即需校验token
+//                .oauth2ResourceServer()
+//                .accessDeniedHandler(new SimpleAccessDeniedHandler())
+//                .authenticationEntryPoint(new SimpleAuthenticationEntryPoint())
+//                .jwt()
+        ;
+        return httpSecurity.build();
+    }
+
+}
